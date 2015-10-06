cbde277107f44692e900e8cb81a1d79295353d11
Anton Nemtsev <thesilentimp@gmail.com>
2015-10-06 18:00:08 +0300

    account

---
+++ source/elements/account/account.js
@@ -0,0 +1,28 @@
+"use strict";
+
+(function() {
+    class Account {
+        /**
+         * @description Start initialization on domload
+         * @constructor
+         */
+        constructor() {
+            let ready = new Promise((resolve, reject) => {
+                if (document.readyState != "loading") return resolve();
+                document.addEventListener("DOMContentLoaded", () => resolve());
+            });
+            ready.then(this.init.bind(this));
+        }
+
+        /**
+         * @description Adding events and properties
+         */
+        init() {
+            this.dirthday = new Pikaday({
+                field: document.querySelector('.account__input_date')
+                , format: 'YYYY-MM-DD'
+            });
+        }
+    }
+    new Account;
+})();
